e1bfb4010da927153b6d776235f0ce172127bf6e
unknown <javierespinmegias@gmail.com>
2020-02-18 18:51:48 +0100

    profile modified

--- app/src/main/java/com/android/myapplication/FragProfile.java
+++ app/src/main/java/com/android/myapplication/FragProfile.java
@@ -3,13 +3,23 @@ package com.android.myapplication;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class FragProfile extends Fragment {
@@ -19,8 +29,12 @@ public class FragProfile extends Fragment {
 
     // TODO: Rename and change types of parameters
     private String mParam1;
-    private EditText name, email, password, city, phone;
+    private TextInputEditText name, email, password, pass2, city, phone;
     private ImageView profile_image;
+    private Button sendChanges;
+
+    public FirebaseDatabase database = FirebaseDatabase.getInstance();
+    public DatabaseReference reference = database.getReference();
 
     private OnFragmentInterfaceCom mListener;
 
@@ -46,8 +60,55 @@ public class FragProfile extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        email = (TextInputEditText) v.findViewById(R.id.email);
+        name = (TextInputEditText) v.findViewById(R.id.name);
+        password = (TextInputEditText) v.findViewById(R.id.pass);
+        pass2 = (TextInputEditText) v.findViewById(R.id.pass2);
+        phone = (TextInputEditText) v.findViewById(R.id.phone);
+        city = (TextInputEditText) v.findViewById(R.id.city);
+
+
+        sendChanges = (Button)v.findViewById(R.id.send_changes);
+
+
+        reference.child("users").child(mParam1).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                CasualUser myUser = dataSnapshot.getValue(CasualUser.class);
+                email.setText(myUser.getEmail());
+                name.setText(myUser.getName());
+                password.setText(myUser.getPass());
+                pass2.setText(myUser.getPass());
+                phone.setText(myUser.getPhone());
+                city.setText(myUser.getCity());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        sendChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CasualUser myNewUser = new CasualUser(
+                        mParam1,
+                        email.getText().toString(),
+                        password.getText().toString(),
+                        name.getText().toString(),
+                        city.getText().toString(),
+                        phone.getText().toString(),
+                        "",
+                        true);
+                reference.child("users").child(mParam1).setValue(myNewUser);
+                Toast.makeText(getContext(), R.string.user_modified, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return v;
     }
 
 
